91c49cc62869eca09dcae5d9a4d479e6bcf81612
kangji <kangji@kangjiui-MacBookPro.local>
2023-09-19 00:32:55 +0900

    #4 put

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from modules import square2  # import로 square 2 모듈을 가져옴
+from modules import square2  # import로 square 2 모듈을 가져옴 # modules 를 폴더를 패키지라고 생각
 
 print(square2.base)
 print(square2.square(4))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional 
 
 app = FastAPI()
 
@@ -23,3 +25,25 @@ async def list_items():
 async def get_item(item_id):
     return {"item_id":item_id}
 
+class Item(BaseModel):
+    name: str # 아무것도 안적으면 required가 됨
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int,item:Item, q:str | None = None):
+    result = {"item_id":item_id, **item.dict()}
+
+    if q:
+        result.update({"q":q})
+    return result
