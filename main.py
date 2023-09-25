ecd55da009670f78310b4ddf4e16338f714e3341
kangji <kangji@kangjiui-MacBookPro.local>
2023-09-26 00:49:49 +0900

    #8

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from enum import Enum
 from fastapi import FastAPI, Query,Path,Body
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from typing import Optional 
 
 app = FastAPI()
 
 # Part 7 Body Multiple parameters
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price : float
-    tax: float | None = None
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price : float
+#     tax: float | None = None
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
+# class User(BaseModel):
+#     username: str
+#     full_name: str | None = None
 
 
 
-@app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int = Path(..., title="The ID of the item ot get", ge=0,le=150),
-    q: str | None = None,
-    item : Item | None = None,
-    user:User,
-    importance : int = Body(...)
-):
-    results ={"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item: 
-        results.update({"item":item})
-    if user:
-        results.update({"user":user})
-    if importance:
-        results.update("importance",importance)
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int = Path(..., title="The ID of the item ot get", ge=0,le=150),
+#     q: str | None = None,
+#     item : Item | None = None,
+#     user:User,
+#     importance : int = Body(...)
+# ):
+#     results ={"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item: 
+#         results.update({"item":item})
+#     if user:
+#         results.update({"user":user})
+#     if importance:
+#         results.update("importance",importance)
 
-    return results
+#     return results
 
 
 
@@ -112,4 +112,15 @@ async def update_item(
 #     if q:
 #         results.update({"q":q})
 #     return results
+
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(None, title="The description of the item", max_length=300)
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
+    tax: float | None = None
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item:Item= Body(..., embed=True)):
+    results = {"item_id": item_id, "item":item}
+    return results
 
