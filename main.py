212c68f29b053b6ce24d1f21f85fe5c195aaaec4
kangji <alleneffort1994@gmail.com>
2023-10-06 01:22:28 +0900

    #9

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 from fastapi import FastAPI, Query,Path,Body
 from pydantic import BaseModel,Field
 from typing import Optional 
@@ -113,14 +114,31 @@ app = FastAPI()
 #         results.update({"q":q})
 #     return results
 
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(None, title="The description of the item", max_length=300)
+#     price: float = Field(..., gt=0, description="The price must be greater than zero")
+#     tax: float | None = None
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item:Item= Body(..., embed=True)):
+#     results = {"item_id": item_id, "item":item}
+#     return results
+
+## part 9 
+class Image(BaseModel):
+    url:str
+    name: str
+
+    
 class Item(BaseModel):
     name: str
-    description: str | None = Field(None, title="The description of the item", max_length=300)
-    price: float = Field(..., gt=0, description="The price must be greater than zero")
+    description: str | None = None
+    price: float
     tax: float | None = None
+    tags: list[str] = []
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item:Item= Body(..., embed=True)):
+@app.put("items/{item_id}")
+async def update_item(item_id:int, item:Item):
     results = {"item_id": item_id, "item":item}
     return results
-
